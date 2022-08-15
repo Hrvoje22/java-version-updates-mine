e04643e109641fd4291e0927082491f38edf80b7
Hrvoje <hbrkljacic@gmail.com>
2022-08-15 10:02:10 -0400

    Commit6

---
+++ m2-enum/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,23 @@
+package com.cydeo;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("How to access a constant?");
+
+        Currency c = Currency.PENNY;
+        System.out.println(c);
+        System.out.println(Currency.PENNY);
+
+        System.out.println("How to get all constants?");
+        Currency[] currencies = Currency.values();
+
+
+        for (Currency currency : currencies){
+            System.out.println(currency);
+        }
+
+
+    }
+}
