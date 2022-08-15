170f52773a6ee5110b14a634803e95a1f2cf1f51
Hrvoje <hbrkljacic@gmail.com>
2022-08-15 10:07:14 -0400

    Commit6

--- m2-enum/src/main/java/com/cydeo/Main.java
+++ m2-enum/src/main/java/com/cydeo/Main.java
@@ -18,6 +18,23 @@ public class Main {
             System.out.println(currency);
         }
 
+        System.out.println("How to use switch-case with enums?");
+
+        switch (Currency.PENNY){
+            case PENNY:
+                System.out.println("It is 1 cent!");
+                break;
+            case NICKLE:
+                System.out.println("It is 5 cent!");
+                break;
+            case DIME:
+                System.out.println("It is 10 cent!");
+                break;
+            case QUARTER:
+                System.out.println("It is 25 cent!");
+                break;
+        }
+
 
     }
 }
