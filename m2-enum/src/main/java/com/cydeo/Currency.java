e6664235ffcf5c9c7b38f6617bb7b407065190cd
Hrvoje <hbrkljacic@gmail.com>
2022-08-15 10:21:37 -0400

    Commit7

--- m2-enum/src/main/java/com/cydeo/Currency.java
+++ m2-enum/src/main/java/com/cydeo/Currency.java
@@ -2,7 +2,19 @@ package com.cydeo;
 
 public enum Currency {
 
-    PENNY, NICKLE, DIME, QUARTER
+    PENNY(1), NICKLE(5), DIME(10), QUARTER(25);
 
     //objects of the currency class
+    //cannot be abstract - it's final
+
+
+    private int value;
+
+    Currency(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
---
+++ m2-enum/src/main/java/com/cydeo/Operation.java
@@ -0,0 +1,12 @@
+package com.cydeo;
+
+public enum Operation {
+
+        PLUS, MINUS, MULTIPLY, DIVIDE;
+
+        Operation() {
+                System.out.println("Constructor");
+        }
+
+        //it will print 4 times - equal to the objects we have
+}
