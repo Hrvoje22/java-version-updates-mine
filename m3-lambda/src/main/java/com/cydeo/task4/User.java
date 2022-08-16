09f9629afc2dec11bdcbb716bfc02d272d7eec6f
Hrvoje <hbrkljacic@gmail.com>
2022-08-16 16:03:50 -0400

    task4

---
+++ m3-lambda/src/main/java/com/cydeo/task4/User.java
@@ -0,0 +1,20 @@
+package com.cydeo.task4;
+
+import jdk.jfr.DataAmount;
+import lombok.*;
+
+//@AllArgsConstructor
+//@Getter
+//@Setter
+//@ToString
+@Data //@Getter @Setter @ToString
+@AllArgsConstructor
+public class User {
+
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+
+}
