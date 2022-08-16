1588f319a38150a3053c0b1b20c5c4ac65d60757
Hrvoje <hbrkljacic@gmail.com>
2022-08-16 14:14:56 -0400

    m3task2

---
+++ m3-lambda/src/main/java/com/cydeo/task3/CheckMember.java
@@ -0,0 +1,7 @@
+package com.cydeo.task3;
+
+public interface CheckMember {
+
+    boolean test(Person p);
+
+}
---
+++ m3-lambda/src/main/java/com/cydeo/task3/MemberTest.java
@@ -0,0 +1,46 @@
+package com.cydeo.task3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MemberTest {
+
+    public static void main(String[] args) {
+
+       Person p1 = new Person("Mike",30,Gender.MALE,"mike@gmail.com");
+       Person p2 = new Person("Ozzy",25,Gender.MALE,"ozzy@gmail.com");
+       Person p3 = new Person("Ally",28,Gender.FEMALE,"Ally@gmail.com");
+       Person p4 = new Person("Diana",20,Gender.FEMALE,"diana@gmail.com");
+
+       List<Person> personList = Arrays.asList(p1,p2,p3,p4);
+
+       CheckMember cm = p -> p.getGender()==Gender.MALE && p.getAge()>=18 && p.getAge()<=25;
+
+       print(personList,cm);
+
+       //or
+
+       print(personList,p -> p.getGender()==Gender.MALE && p.getAge()>=18 && p.getAge()<=25);
+
+
+
+
+
+    }
+
+    private static void print(List<Person> personList, CheckMember member){
+
+        List<Person> result = new ArrayList<>();
+
+        for(Person person : personList){
+            if(member.test(person)){
+                result.add(person);
+            }
+        }
+
+        System.out.println(result);
+
+    }
+
+}
