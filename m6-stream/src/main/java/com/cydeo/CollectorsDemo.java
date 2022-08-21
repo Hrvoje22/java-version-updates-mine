d3f62775414d3fa1c48486de373ff0a2955501c4
Hrvoje <hbrkljacic@gmail.com>
2022-08-21 14:55:10 -0400

    Collectors

--- m6-stream/src/main/java/com/cydeo/CollectorsDemo.java
+++ m6-stream/src/main/java/com/cydeo/CollectorsDemo.java
@@ -1,5 +1,8 @@
 package com.cydeo;
 
+import com.cydeo.task.task2.Dish;
+import com.cydeo.task.task2.DishData;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -21,6 +24,35 @@ public class CollectorsDemo {
                 .filter(x -> x%2 == 0)
                 .collect(Collectors.toCollection(HashSet::new));
         System.out.println(numberSet);
+
+        //toList() : returns a Collector interface that gathers the input data into a new List
+
+        List<Integer> numberList2 = numbers.stream()
+                .filter(x -> x%2 == 0)
+                .collect(Collectors.toList());
+        System.out.println(numberList2);
+
+        //toSet() : returns a Collector interface that gathers the input data into a new Set
+
+        Set<Integer> numberSet2 = numbers.stream()
+                .filter(x -> x%2 == 0)
+                .collect(Collectors.toSet());
+        System.out.println(numberSet2);
+
+        //toMap(Function,Function) : returns a Collector interface that gathers the input data into a new map
+
+        Map<String,Integer> dishMap = DishData.getAll().stream()
+                .collect(Collectors.toMap(Dish::getName,Dish::getCalories));
+        System.out.println(dishMap);
+
+        //summingInt(ToIntFunction) : returns a Collector that produces the sum of a Integer - valued function
+        Integer sum = DishData.getAll().stream()
+                .collect(Collectors.summingInt(Dish::getCalories));
+        System.out.println(sum);
+
+
+
+
 
 
 
