084ae7ee9b01c93407d1901a165f114a75dd3bb9
Hrvoje <hbrkljacic@gmail.com>
2022-08-23 15:13:04 -0400

    Java14

---
+++ m8-new_version_updates/src/main/java/com/cydeo/java14/User.java
@@ -0,0 +1,19 @@
+package com.cydeo.java14;
+
+public record User(String name, String address) { //This is a constructor
+
+    //We have now:
+    //All Arg const
+    //Getter
+    //To string
+    //Equals
+    //Hash Code
+
+    //ALL THE FIELDS ARE PRIVATE FINAL
+
+    //No Setters :(
+
+
+
+
+}
